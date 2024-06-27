d80c3869623dfea9e5bfdc275afb43a671046dd3
de-ahsan <mail.ahsan.ellahi@gmail.com>
2024-06-27 22:50:49 +0500

    making wsgi configurations

--- manage.py
+++ manage.py
@@ -4,10 +4,11 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphiql.settings")
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'graphiql.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
