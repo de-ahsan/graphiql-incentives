6a35f981eefbf98c8ab00cb1abc82fe3550fcc8c
de-ahsan <mail.ahsan.ellahi@gmail.com>
2024-06-28 17:10:40 +0500

    renamed app to graphql

--- graphql/wsgi.py
+++ graphql/wsgi.py
@@ -3,7 +3,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphiql.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphql.settings")
 
 application = get_wsgi_application()
 app = application  # Alias for Vercel
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphiql.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphql.settings")
 
 
 def main():
